ce6d04230137bd63f3f853e9c0e5b37af2278b7c
eapenzacharias <eapenzac@gmail.com>
2020-03-11 16:21:43 +0100

    Code Quality Improvements reduced lines of code in calculate

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,6 +1,26 @@
 /* eslint-disable no-console */
 import operate from './operate';
 
+function negate(num) {
+  let next = num;
+  if (!next) {
+    next = '-';
+  } else {
+    next = `-${next}`;
+  }
+  return next;
+}
+
+function number(num, btn) {
+  let next = num;
+  if (!next) {
+    next = btn;
+  } else {
+    next += btn;
+  }
+  return next;
+}
+
 function calculate(btn, calculator) {
   let { total, next, operation } = calculator;
   switch (btn) {
@@ -10,37 +30,16 @@ function calculate(btn, calculator) {
       operation = null;
       break;
     case '+/-':
-      if (!next) {
-        next = '-';
-      } else {
-        next = `-${next}`;
-      }
+      next = negate(next);
       break;
-    case '+':
-    case '-':
-    case 'x':
-    case '/':
-    case '%':
+    case '+': case '-': case 'x': case '/': case '%':
       operation = btn;
       total = next;
       next = null;
       break;
-    case '0':
-    case '1':
-    case '2':
-    case '3':
-    case '4':
-    case '5':
-    case '6':
-    case '7':
-    case '8':
-    case '9':
-    case '.':
-      if (!next) {
-        next = btn;
-      } else {
-        next += btn;
-      }
+    case '0': case '1': case '2': case '3': case '4': case '5':
+    case '6': case '7': case '8': case '9': case '.':
+      next = number(next, btn);
       break;
     case '=':
       total = operate(total, next, operation).toString();
