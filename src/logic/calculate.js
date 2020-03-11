ee92a3e64d5cb0a8dc5162b5e99126d304d34892
eapenzacharias <eapenzac@gmail.com>
2020-03-11 17:05:52 +0100

    Code Quality Improvements - reduced lines of code in calculate.js

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -25,17 +25,13 @@ function calculate(btn, calculator) {
   let { total, next, operation } = calculator;
   switch (btn) {
     case 'AC':
-      total = '0';
-      next = null;
-      operation = null;
+      [total, next, operation] = ['0', null, null];
       break;
     case '+/-':
       next = negate(next);
       break;
     case '+': case '-': case 'x': case '/': case '%':
-      operation = btn;
-      total = next;
-      next = null;
+      [total, next, operation] = [next, null, btn];
       break;
     case '0': case '1': case '2': case '3': case '4': case '5': case '6': case '7': case '8': case '9': case '.':
       next = number(next, btn);
